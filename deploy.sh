1995a8defdbf119b740ef530c4f943cc9108ee3b
bsnizek <bs@metascapes.org>
2021-03-25 10:57:50 +0100

    Deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-npm run build
-git --work-tree dist add --all
-git --work-tree dist commit -m 'Deploy'
-git push origin HEAD:gh-pages --force
-rm -r dist
-git checkout -f master
-git branch -D gh-pages
